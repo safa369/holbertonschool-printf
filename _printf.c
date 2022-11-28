36d51b9b9df644c7c88042e24ca49322b05f157a
user.name <safa.riahi1992@hotmail.com>
2022-11-28 02:23:57 -0800

    my own printf

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
 	va_list argm;
-	int len, i, j;
+	int len, i, j, k;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
@@ -17,20 +17,22 @@ int _printf(const char *format, ...)
 	i = len = 0;
 	while (format[i] != '\0' && format != NULL)
 		{
-			if(format[i] != '%')
+			k = 0;
+			if(format[i] != '%' && format[i] != 'c' && format[i] != 's')
 			{
 				_putchar(format[i]);
 				len++;
+				k = 1;
 			}
-			if (format[i] == '%')
+			if (format[i] == '%' && k == 0)
 			{
 				i++;
 				j = get_function(format[i], argm);
-				if (j != 0)
+				if (j != 0 )
 				{
 					len = len + j;
 					i++;
-					continue;
+
 				}
 				else if (format[i] == '\0')
 				{
