a7b855e883c7c2b91dd182e38f2eef974f986ecb
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2023-01-04 15:11:08 +0100

    header file

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef print_f
 #define print_f
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include<unistd.h>
+#include <limits.h>
+
 /**
  * struct printing - structur for compiling function.
  * @prt: structur.
@@ -8,12 +13,13 @@
  */
 typedef struct printing
 {
-	char prt;
-	int (*ptr)();
-}print;
-int get_function(const char c, va_list p);
+	char *prt;
+	int (*ptr)(va_list p);
+}print_t;
+int get_function(int *i,const char *format, va_list argmt);
 int _printf(const char *format, ...);
-int print_char(va_list argm);
-int print_string(va_list argm);
+int print_char(va_list p);
+int print_string(va_list p);
 int _putchar(char c);
+int print_integer(va_list argmt);
 #endif
